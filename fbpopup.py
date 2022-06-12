792e53d72654e9a7307a9ea27ea0b107163ffc66
Faisal Ahmed <faisalahmed0086@gmail.com>
2022-06-12 16:23:03 +0600

    Add files via upload

---
+++ fbpopup.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.expected_conditions import alert_is_present
+
+chrome_options = Options()
+chrome_options.add_argument("--disable-notifications")
+
+driver=webdriver.Chrome(executable_path="C:\\python_selenium\\chromedriver_win32\\chromedriver.exe", chrome_options=chrome_options)
+driver.get("http://www.facebook.com")
+driver.find_element_by_name("email").send_keys("email")
+driver.find_element_by_name("pass").send_keys("pass")
+driver.find_element_by_name("login").click()
+print("FB")
